2d943f6a14c1a39f88adac133e15fd9315cbf592
Tallinn Splinter <tallinnsplinter@gmail.com>
2026-09-21 16:13:25 -0400

    Completed class 1 assignment and README

--- 01-inclass.R
+++ 01-inclass.R
@@ -29,9 +29,30 @@ library(tidyverse)
 install.packages("flextable")
 library(flextable)
 
-yrbss$Grade <- yrbss$grade
-yrbss$Gender <- yrbss$gender
 
+#reorder grade to increasing order
+yrbss$Grade <- factor(
+  yrbss$Grade,
+  levels = c("9", "10", "11", "12", "other")
+)
+
+#check
+table(yrbss$Grade)
+
+### change capital letters in category labels
+
+yrbss <- yrbss %>% 
+  mutate(Gender = fct_relabel(Gender, str_to_title))
+print(levels(yrbss$Gender))
+
+#change all coloumn names
+names(yrbss) <- stringr::str_to_title(names(yrbss))
+
+# category lables
+yrbss <- yrbss %>%
+  mutate(across(where(is.character), ~ stringr::str_to_sentence(.)))
+
+## summary table
 z <- summarizor(
   yrbss[c("Grade", "Gender")],
   overall_label = NULL
@@ -42,6 +63,13 @@ ft_1
 
 # To understand the pattern of physical activity by grade and gender,
 # 1) aggregate  `physically_active_7d` by calculating its mean within each grade and gender
+aggregate(Physically_active_7d ~ Grade + Gender, data=yrbss, FUN=mean)
+
+f <- aggregate(Physically_active_7d ~ Grade + Gender, data=yrbss, FUN=mean)
+table_activity_gender_by_grade <- as_flextable(f)
+table_activity_gender_by_grade
+
+
 # 2) create a plot showing the average number of physically active days
 #      x-axis: grade
 #      y-axis: Mean of `physcially_active_7d`
@@ -50,16 +78,33 @@ ft_1
 # no one correct way to do this
 # Ensure that the figure is clearly labeled and includes an appropriate legend
 
-aggregate(xxx) |>
-  ggplot(aes(xxx)) + 
-  geom_line()
-...
+##Plot average physically active days by grade and gender
+activity_grade_plot <- aggregate(Physically_active_7d ~ Grade + Gender, data=yrbss, FUN=mean, na.rm=TRUE) |>
+  ggplot(aes(x=Grade, y= Physically_active_7d, color=Gender, group=Gender)) + 
+  geom_line()+ labs(title = "Physical Activity by Grade",x="Grade", y="Average Physical Activity")
 
+activity_grade_plot
 
+# Question 3
 # Create a plot that shows the relationship betwen physical activity and bmi
 # among female students in grade 12 
 # Ensure that the figure is clearly labeled and includes an appropriate legend
 
+#create BMI label
+data <- yrbss
+data$BMI <-  data$Weight / (data$Height)^2
 
+#plot BMI by activity 
 
+BMI_plot <- data |>
+  subset(Gender == "Female" & Grade == "12") |>
+  ggplot(aes(x = BMI, y = Physically_active_7d)) +
+  geom_point(alpha = 0.4) +
+  geom_smooth(method = "lm", se = TRUE) +
+  labs(
+    title = "Physical Activity and BMI Among Grade 12 Female Students",
+    x = "BMI",
+    y = "# of Physically Active days"
+  )
+BMI_plot
 # Push your completed code to your GitHub repository
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 README file for September 21
+This repository contains the assignment for class 1 of CHL5233. 
+01-inclass.R contains the in class assignments where the yrbss package was used to 
+- Reorder "Grade" to inceasing order
+- Re-label all category labels to start with a capital letter
+- Created a table showing the average physical Acitivity in the past 7 days by grade and gender
+- Plotted the average number of physically active days by grade, with lines for each Gender
+- Created a BMI variable and plotted that by physical activity in Grade 12 Female students
